6ec4fe006d8adf557c470dd011f0594bdcebaf25
Gurmeet Athwal <gurmeet.athwal@gmail.com>
2023-09-21 11:42:18 +0530

    Add code and features

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -9,6 +9,13 @@ class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class ChoiceInline(admin.StackedInline):    # Choice Inline
+    model = Choice
+    extra = 2
+
+class QuestionInline(admin.StackedInline):  # Question Inline
+    model = Question
+    extra = 2
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -21,6 +28,10 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):      # Question Admin model
+    inlines = [ChoiceInline]
+    list_display = ['content']
+
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +39,6 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)  # Register Questions Model
+admin.site.register(Choice)                   # Register Choice Model
+admin.site.register(Submission)               # Register Submission Model
